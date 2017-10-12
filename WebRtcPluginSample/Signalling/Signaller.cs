c8e8fa540d9e136ada9e2b63071db7817f8142db
dyama7 <7d.yama@gmail.com>
2017-10-13 08:11:19 +0900

    3rd

--- WebRtcPluginSample/Signalling/Signaller.cs
+++ WebRtcPluginSample/Signalling/Signaller.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace WebRtcPluginSample.Signalling
 {
+    public delegate void SignedInDelegate();
+    public delegate void DisconnectedDelegate();
+    public delegate void PeerConnectedDelegate(int id, string name);
+    public delegate void PeerDisconnectedDelegate(int peerId);
+    public delegate void PeerHangupDelegate(int peerId);
+    public delegate void MessageFromPeerDelegate(int peerId, string message);
+    public delegate void MessageSentDelegate(int err);
+    public delegate void ServerConnectionFailureDelegate();
+
     public class Signaller
     {
+        public event SignedInDelegate OnSignedIn;
+        public event DisconnectedDelegate OnDisconnected;
+        public event PeerConnectedDelegate OnPeerConnected;         // リモートユーザがシグナリングサーバに接続してきたときのイベント
+        public event PeerDisconnectedDelegate OnPeerDisconnected;   // リモートユーザがシグナリングサーバからログアウトしたときのイベント
+        public event PeerHangupDelegate OnPeerHangup;
+        public event MessageFromPeerDelegate OnMessageFromPeer;
+        public event ServerConnectionFailureDelegate OnServerConnectionFailure;
     }
 }
--- WebRtcPluginSample/WebRtcControl.cs
+++ WebRtcPluginSample/WebRtcControl.cs
@@ -8,6 +8,9 @@ using Windows.UI.Core;
 using Org.WebRtc;
 using WebRtcPluginSample.Signalling;
 using System.Linq;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using WebRtcPluginSample.Model;
 
 namespace WebRtcPluginSample
 {
@@ -46,11 +49,247 @@ namespace WebRtcPluginSample
                 AudioPlayoutDevices.Add(audioPlayoutDevice);
             }
 
+            // 各種メディアデバイスはリストの先頭のものを使用する
+            // Holoはいいけど、Immersiveの場合は考え直すべきです
+            if(SelectedCamera == null && Cameras.Count > 0)
+            {
+                SelectedCamera = Cameras.First();
+            }
+            
+            if(SelectedMicrophone == null && Microphones.Count > 0)
+            {
+                SelectedMicrophone = Microphones.First();
+            }
 
+            if(SelectedAudioPlayoutDevice == null && AudioPlayoutDevices.Count >0)
+            {
+                SelectedAudioPlayoutDevice = AudioPlayoutDevices.First();
+            }
 
+            // マシンに接続されたメディアデバイスが変更されたときのイベントハンドラ
+            Conductor.Instance.Media.OnMediaDevicesChanged += OnMediaDeviceChanged;
+            // リモートユーザがシグナリングサーバに接続してきたときのハンドラ
+            // 自分の初回ログイン、ポーリング時の新規ユーザ追加時にコールされる
+            // TODO 選択方法を工夫したいところ
+            Conductor.Instance.Signaller.OnPeerConnected += (peerId, peerName) =>
+            {
+                // リモートユーザのリストを行進する
+                if (Peers == null)
+                {
+                    Peers = new List<Peer>();
+                    Conductor.Instance.Peers = Peers;
+                }
+                Peers.Add(new Peer { Id = peerId, Name = peerName });
+                // 接続してきたリモートユーザをPeer候補とする
+                SelectedPeer = Peers.First(x => x.Id == peerId);
+            };
+            // リモートユーザがシグナリングサーバからログアウトしたときのハンドラ
+            Conductor.Instance.Signaller.OnPeerDisconnected += (peerId) =>
+            {
+                var peerToRemove = Peers?.FirstOrDefault(p => p.Id == peerId);
+                if (peerToRemove != null)
+                    Peers.Remove(peerToRemove);
+            };
+            // シグナリングサーバへの接続が完了したときのハンドラ
+            Conductor.Instance.Signaller.OnSignedIn += () =>
+            {
+                IsConnected = true;
+                IsMicrophoneEnabled = false;
+                IsCameraEnabled = false;
+                IsConnecting = false;
+
+                OnStatusMessageUpdate?.Invoke("Signed in");
+            };
+            // シグナリングサーバへの接続が失敗したときのハンドラ
+            Conductor.Instance.Signaller.OnServerConnectionFailure += () =>
+            {
+                IsConnecting = false;
+
+                OnStatusMessageUpdate?.Invoke("Server Connection Failure");
+            };
+            // シグナリングサーバからログアウトしたときのハンドラ
+            Conductor.Instance.Signaller.OnDisconnected += () =>
+             {
+                 IsConnected = false;
+                 IsMicrophoneEnabled = false;
+                 IsCameraEnabled = false;
+                 IsDisconnecting = false;
+                 Peers?.Clear();
+
+                 OnStatusMessageUpdate?.Invoke("Disconnected");
+             };
 
         }
 
+        #region Event Handlers
+        /// <summary>
+        /// マシンに接続されたメディアデバイスが変更されたときのハンドラ。
+        /// メディアデバイスのホットプラグに対応する。
+        /// </summary>
+        /// <param name="mediaType">接続/切断されたメディアデバイスのメディアタイプ</param>
+        private void OnMediaDeviceChanged(MediaDeviceType mediaType)
+        {
+            switch (mediaType)
+            {
+                // カメラ
+                case MediaDeviceType.MediaDeviceType_VideoCapture:
+                    RefreshVideoCaptureDevice(Conductor.Instance.Media.GetVideoCaptureDevices());
+                    break;
+                // マイク
+                case MediaDeviceType.MediaDeviceType_AudioCapture:
+                    RefreshAudioCaptureDevices(Conductor.Instance.Media.GetAudioCaptureDevices());
+                    break;
+                // スピーカー
+                case MediaDeviceType.MediaDeviceType_AudioPlayout:
+                    RefreshAudioPlayoutDevices(Conductor.Instance.Media.GetAudioPlayoutDevices());
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 利用可能ビデオデバイスのリストをリフレッシュする
+        /// </summary>
+        /// <param name="videoCaptureDevices">新しく取得した利用可能ビデオデバイスのリスト</param>
+        private void RefreshVideoCaptureDevice(IList<MediaDevice> videoCaptureDevices)
+        {
+            RunOnUiThread(() =>
+            {
+                // 削除対象のコレクション
+                Collection<MediaDevice> videoCaptureDevicesToRemove = new Collection<MediaDevice>();
+                // ViewModelが現在保有しているリストと新しいリストを突き合わせて、新しいリストに存在しない場合は削除対象のコレクションにセットする
+                foreach (MediaDevice videoCaptureDevice in Cameras)
+                {
+                    if (videoCaptureDevices.FirstOrDefault(x => x.Id == videoCaptureDevice.Id) == null)
+                    {
+                        videoCaptureDevicesToRemove.Add(videoCaptureDevice);
+                    }
+                }
+                // 削除対象がfixしたので、ViewModelが管理するリストから削除し、Viewにも反映
+                foreach (MediaDevice removedVideoCaptureDevice in videoCaptureDevicesToRemove)
+                {
+                    if (SelectedCamera != null && SelectedCamera.Id == removedVideoCaptureDevice.Id)
+                    {
+                        // 現在選択中のカメラが削除されたのならば、いったんnullにしておく
+                        SelectedCamera = null;
+                    }
+                    // CamerasはObservableCollectionなのでViewへ通知が行く
+                    Cameras.Remove(removedVideoCaptureDevice);
+                }
+                // 新しいリストの要素がViewModelが現在保有するリストになければ追加する
+                foreach (MediaDevice videoCaptureDevice in videoCaptureDevices)
+                {
+                    if (Cameras.FirstOrDefault(x => x.Id == videoCaptureDevice.Id) == null)
+                    {
+                        // CamerasはObservableCollectionなのでViewへ通知が行く
+                        Cameras.Add(videoCaptureDevice);
+                    }
+                }
+
+                // 選択中のカメラが削除されたのならば、リストの先頭のものをセット
+                if (SelectedCamera == null)
+                {
+                    SelectedCamera = Cameras.FirstOrDefault();
+                }
+            });
+        }
+
+        /// <summary>
+        /// オーディオデバイス一覧のリフレッシュ
+        /// </summary>
+        /// <param name="audioCaptureDevices"></param>
+        private void RefreshAudioCaptureDevices(IList<MediaDevice> audioCaptureDevices)
+        {
+            RunOnUiThread(() =>
+            {
+                var selectedMicrophoneId = SelectedMicrophone?.Id;
+                SelectedMicrophone = null;
+                Microphones.Clear();
+                foreach (MediaDevice audioCaptureDevice in audioCaptureDevices)
+                {
+                    Microphones.Add(audioCaptureDevice);
+                    if (audioCaptureDevice.Id == selectedMicrophoneId)
+                    {
+                        SelectedMicrophone = Microphones.Last();
+                    }
+                }
+
+                if (SelectedMicrophone == null)
+                {
+                    SelectedMicrophone = Microphones.First();
+                }
+            });
+        }
+
+        /// <summary>
+        /// スピーカーデバイスのリフレッシュ
+        /// </summary>
+        /// <param name="audioPlayoutDevices"></param>
+        private void RefreshAudioPlayoutDevices(IList<MediaDevice> audioPlayoutDevices)
+        {
+            RunOnUiThread(() =>
+            {
+                var selectedPlayoutDeviceId = SelectedAudioPlayoutDevice?.Id;
+                SelectedAudioPlayoutDevice = null;
+                AudioPlayoutDevices.Clear();
+                foreach (MediaDevice audioPlayoutDevice in audioPlayoutDevices)
+                {
+                    AudioPlayoutDevices.Add(audioPlayoutDevice);
+                    if (audioPlayoutDevice.Id == selectedPlayoutDeviceId)
+                    {
+                        SelectedAudioPlayoutDevice = audioPlayoutDevice;
+                    }
+                }
+
+                if (SelectedAudioPlayoutDevice == null)
+                {
+                    SelectedAudioPlayoutDevice = AudioPlayoutDevices.FirstOrDefault();
+                }
+            });
+        }
+
+
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// リモートユーザのリスト
+        /// </summary>
+        public List<Peer> Peers { get; set; }
+
+        /// <summary>
+        /// シグナリングサーバへの接続が完了しているかどうか
+        /// </summary>
+        public bool IsConnected { get; set; }
+
+        /// <summary>
+        /// マイクの使用可否
+        /// </summary>
+        public bool IsMicrophoneEnabled { get; set; }
+
+        /// <summary>
+        /// カメラの使用可否
+        /// </summary>
+        public bool IsCameraEnabled { get; set; }
+
+        /// <summary>
+        /// シグナリングサーバへ接続中かどうか
+        /// </summary>
+        public bool IsConnecting { get; set; }
+
+        /// <summary>
+        /// シグナリングサーバから切断中かどうか
+        /// </summary>
+        public bool IsDisconnecting { get; set; }
+
+        /// <summary>
+        /// 選択中のリモートユーザ
+        /// </summary>
+        public Peer SelectedPeer { get; set; }
+
+
+        #endregion
+
+        #region MediaDevice Settings
         /// <summary>
         /// マシン上のカメラデバイスのリスト
         /// </summary>
@@ -198,6 +437,44 @@ namespace WebRtcPluginSample
         }
         private CaptureCapability _selectedCapFpsItem;
 
+        /// <summary>
+        /// 現在選択中のマイクデバイス
+        /// </summary>
+        public MediaDevice SelectedMicrophone {
+            get => _selectedMicrophone;
+            set {
+                if(_selectedMicrophone != value)
+                {
+                    _selectedMicrophone = value;
+                    if(value != null)
+                    {
+                        // 使用するマイクデバイスを変更する
+                        Conductor.Instance.Media.SelectAudioCaptureDevice(_selectedMicrophone);
+                    }
+                }
+            }
+        }
+        private MediaDevice _selectedMicrophone;
+
+        /// <summary>
+        /// 現在選択中のスピーカーデバイス
+        /// </summary>
+        public MediaDevice SelectedAudioPlayoutDevice {
+            get => _selectedAudioPlayoutDevice;
+            set {
+                if(_selectedAudioPlayoutDevice != value)
+                {
+                    _selectedAudioPlayoutDevice = value;
+                    if(value != null)
+                    {
+                        // 使用するスピーカーデバイスを変更する
+                        Conductor.Instance.Media.SelectAudioPlayoutDevice(_selectedAudioPlayoutDevice);
+                    }
+                }
+            }
+        }
+        private MediaDevice _selectedAudioPlayoutDevice;
+        #endregion
 
         private void RunOnUiThread(Action fn)
         {
